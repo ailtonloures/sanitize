e2ccce4913f935e9ba1e3ad39b578c0fffbd5d1e
Ailton Loures <ailton.loures99@gmail.com>
2020-06-27 17:47:52 -0300

    iniciando projeto

---
+++ api/sanitize.js
@@ -0,0 +1,92 @@
+/**
+ * Created by Ailton Loures on 27/06/2020 
+ * 
+ * @requires JQuery-v3.5.1
+ * @version 0.1
+ * @author Ailton Loures <ailton.loures99@gmail.com>
+ */
+(function ($) {
+
+    /**
+     * Automatize form submits
+     * 
+     * @author Ailton Loures <ailton.loures99@gmail.com>
+     * 
+     * @param {Object} callbacks { success : callable, error : callable, beforeSend: callable, complete: callable }
+     * @returns {HTMLFormElement} 
+     */
+    $.fn.formAjaxSubmit = function ({
+        success,
+        error,
+        beforeSend,
+        complete
+    }) {
+
+        const _form = $(this);
+
+        _form
+            .on("submit", function (event) {
+                event.preventDefault();
+
+                const method = _form.attr('method');
+                const actionUrl = _form.attr('action');
+                const enctype = _form.attr('enctype');
+
+                if (enctype && enctype === "multipart/form-data")
+                    var formData = new FormData(_form[0]);
+                else {
+                    if (method === "GET")
+                        var formData = _form.serialize();
+
+                    if (method === "POST")
+                        var formData = _form.serializeArray();
+                }
+
+                $.ajax({
+                    type: method,
+                    url: actionUrl,
+                    data: formData,
+                    success: function (response) { success && success(response) },
+                    error: function (reject) { error && error(reject) },
+                    beforeSend: function () { beforeSend && beforeSend(_form) },
+                    complete: function () { complete && complete() }
+                });
+
+            });
+
+        return _form;
+    }
+
+    /**
+     * Append small messages after elements of the form
+     * 
+     * @author Ailton Loures <ailton.loures99@gmail.com>
+     * 
+     * @param {Object} messages the messages in object
+     * @param {Object} strings { classMessage : Array|String , styleMessage : Array|String } 
+     * @returns {HTMLFormElement}
+     */
+    $.fn.formAppendMessages = function (messages, { classMessage, styleMessage }) {
+        const _form = $(this);
+
+        _form.find("small").remove();
+
+        $.each(messages, function (target, value) {
+            $.each(value, function (name, message) {
+                _form
+                    .find(`[name="${target}"]`)
+                    .after(
+                        `<small 
+                            style="${styleMessage instanceof Array ? styleMessage.join(";") : (styleMessage || "")}"
+                            class="${classMessage instanceof Array ? classMessage.join(" ") : (classMessage || "")}">
+                            ${message}
+                        </small>`
+                    );
+            });
+        });
+
+        return _form;
+    }
+
+})(jQuery);
+
